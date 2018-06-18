429c3dc04dbbb47c27369c729de2ea324e9b0140
ashwinibm <ashwini@baysidetech.in>
2018-06-18 18:47:04 +0530

    Integrated bootstrap

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route("/")
 @app.route('/index')
 def hello():
-    return render_template('index.html', greeting='Hello World!')
+  return render_template('index.html')
