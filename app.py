d7df7c0853682b3807f216c5806ab10fbc3ab7ac
ashwinibm <ashwini@baysidetech.in>
2018-06-20 15:18:01 +0530

    API for tag

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ def allowed_file(filename):
 def index():
   return render_template('index.html')
 
-@app.route('/tag', methods=['POST', 'GET'])
-def get_tags():
+@app.route('/tag', methods=['POST'])
+def tagit():
   if 'file' not in request.files:
     return render_template('index.html', error="No file part")
 
@@ -37,3 +37,24 @@ def get_tags():
     return render_template('index.html', error="File type not allowed")
 
   return render_template('index.html')
+
+@app.route('/api/tag', methods=['POST'])
+def tagit_api():
+  print(len(request.files))
+  if 'file' not in request.files:
+    return jsonify(error="No file part"), 400
+
+  file = request.files['file']
+  if file.filename == '':
+    return jsonify(error="No Selected File"), 400
+
+  if allowed_file(file.filename):
+    filename = secure_filename(file.filename)
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    file.save(filepath)
+    tags = tag_doc(filepath)
+    return jsonify(tags=tags), 200
+  else:
+    return jsonify(error="File type not allowed"), 400
+
+  return jsonify(), 204
